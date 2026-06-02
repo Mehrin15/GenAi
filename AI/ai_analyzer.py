d74e9600c5f5818ab09861a215e11d47b6f73a61
Mehrin15 <mehrinfathim1515@gmail.com>
2026-06-02 22:24:20 +0530

    Updated AI analyzer to support dynamic PDF input

--- AI/ai_analyzer.py
+++ AI/ai_analyzer.py
@@ -1,16 +1,22 @@
 import google.generativeai as genai
 from pdf_extractor import extract_text
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
 API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
-# Extract text from PDF
-question_text = extract_text("sample_question_paper.pdf")
 
-prompt = f"""
+def analyze_question_paper(pdf_path):
+
+    question_text = extract_text(pdf_path)
+
+    prompt = f"""
 Analyze the question paper.
 
 Return ONLY valid JSON.
@@ -36,6 +42,11 @@ Question Paper:
 {question_text}
 """
 
-response = model.generate_content(prompt)
+    response = model.generate_content(prompt)
 
-print(response.text)
+    return response.text
+
+
+if __name__ == "__main__":
+    result = analyze_question_paper("sample_question_paper.pdf")
+    print(result)
